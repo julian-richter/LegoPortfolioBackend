3d450ebc94c6dda068fdc9cb8c45721b2bef894a
Julian Richter <sight.sodden.1w@icloud.com>
2025-11-03 23:59:00 +0100

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module LegoManagerAPI
+
+go 1.25
---
+++ internal/config/database/databaseConfig.go
@@ -0,0 +1,13 @@
+package database
+
+// DatabaseConfig type for database configuration
+type DatabaseConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+	MaxConns int
+	MinConns int
+}
